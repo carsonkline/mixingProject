f129e35c68fba31e1c9b8344848bd47868f48ada
carsonkline <carson.kline20@gmail.com>
2022-03-23 09:16:53 -0800

    Fixed ingredient sorting

--- src/app/drinklist/drinklist.component.ts
+++ src/app/drinklist/drinklist.component.ts
@@ -12,12 +12,23 @@ import { Ingredient } from '../ingredient';
   styleUrls: ['./drinklist.component.css']
 })
 export class DrinklistComponent implements OnInit {
-  recipes = storedRecipes;
+  recipes: Recipe[] = storedRecipes;
   selectedRecipe?: Recipe;
   ingredients = storedIngredients;
-  selectedIngredient?: Ingredient;
 
-  selectedIngredients: Ingredient[] = [];
+
+  selectedIngredients: number[] = [];
+
+  get filteredRecipe(): Recipe[] {
+    /* This fuction filters the recipe array to diplay recipies based on availible ingredients */
+    return this.recipes.filter(recipe => {
+      for (let ingredient of recipe.ingredients) {
+        if (this.selectedIngredients.includes(ingredient) == false)
+          return false
+      }
+      return true
+    })
+  }
 
   constructor() { }
   ngOnInit(): void {
@@ -26,10 +37,16 @@ export class DrinklistComponent implements OnInit {
     this.selectedRecipe = recipe;
   }
   onSelectIngredient(ingredient: Ingredient): void {
-    this.selectedIngredient = ingredient;
-    this.selectedIngredients.push(ingredient);
-    this.selectedRecipe = this.recipes.find(recipe=>{
-      return this.selectedIngredients.some(ingredient=>recipe.ingredients.includes(ingredient.id))
-    })
+    /*  Adds or Removes  */
+    if (this.selectedIngredients.includes(ingredient.id)) {
+      const index = this.selectedIngredients.indexOf(ingredient.id);
+      if (index > -1) {
+        this.selectedIngredients.splice(index, 1); // 2nd parameter means remove one item only
+      }
+    }
+    else {
+      this.selectedIngredients.push(ingredient.id);
+    }
+
   }
 }
